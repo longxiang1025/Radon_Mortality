968cd2f6765fd88199a4b596f31844d7b854e873
Longxiang Li <loli@boslogin02.rc.fas.harvard.edu>
2020-10-09 00:38:30 -0400

    Recreate the lab specific training dataset

--- Code/02_Extact_ZIp_Rn_Data.R
+++ Code/02_Extact_ZIp_Rn_Data.R
@@ -6,98 +6,131 @@ library(ggplot2)
 library(lubridate)
 library(raster)
 
-radon_data<-read.csv("/n/koutrakis_lab/lab/Group_Data/Radon/harvard.csv",header=T,sep = "\t")
+nc_lab_data<-read.csv("/n/koutrakis_lab/lab/Group_Data/Radon/harvard.csv",header=T,sep = "\t")
 ma_lab_data<-read.csv("/n/koutrakis_lab/lab/Group_Data/Radon/HSPH_Export_MA_190628.csv",header=T)
 ma_lab_data$TestPostalCode=as.character(ma_lab_data$TestPostalCode)
 pa_lab_data<-read.csv("/n/koutrakis_lab/lab/Group_Data/Radon/HSPH_Export_PA_190628.csv",header=T)
 pa_lab_data$TestPostalCode<-as.character(pa_lab_data$TestPostalCode)
-radon_data_sec<-bind_rows(ma_lab_data,pa_lab_data)
 
 load(here::here("Data","GeoData","2015_Shapes.RData"))
 load(here::here("Data","GeoData","Boundaries.RData"))
 
 #clean the date data of NC lab
-radon_data$STARTDATE=as.character(radon_data$STARTDATE)
-radon_data$ENDDATE=as.character(radon_data$ENDDATE)
+nc_lab_data$STARTDATE=as.character(nc_lab_data$STARTDATE)
+nc_lab_data$ENDDATE=as.character(nc_lab_data$ENDDATE)
 
-radon_data<-radon_data%>%mutate(STARTDATE=ifelse(STARTDATE=="00/00/0000",NA,STARTDATE))
-radon_data<-radon_data%>%filter(!is.na(STARTDATE))
-radon_data$STARTDATE<-as.Date(radon_data$STARTDATE,"%m/%d/%Y")
+nc_lab_data<-nc_lab_data%>%mutate(STARTDATE=ifelse(STARTDATE=="00/00/0000",NA,STARTDATE))
+nc_lab_data<-nc_lab_data%>%filter(!is.na(STARTDATE))
+nc_lab_data$STARTDATE<-as.Date(nc_lab_data$STARTDATE,"%m/%d/%Y")
 
-radon_data<-radon_data%>%mutate(ENDDATE=ifelse(ENDDATE=="00/00/0000",NA,ENDDATE))
-radon_data<-radon_data%>%filter(!is.na(ENDDATE))
-radon_data$ENDDATE<-as.Date(radon_data$ENDDATE,"%m/%d/%Y")
+nc_lab_data<-nc_lab_data%>%mutate(ENDDATE=ifelse(ENDDATE=="00/00/0000",NA,ENDDATE))
+nc_lab_data<-nc_lab_data%>%filter(!is.na(ENDDATE))
+nc_lab_data$ENDDATE<-as.Date(nc_lab_data$ENDDATE,"%m/%d/%Y")
 
-radon_data$ZIPCODE=substr(radon_data$POSTALCODE,1,5)
-radon_data$PCI.L=as.numeric(as.character(radon_data$PCI.L))
-radon_data=radon_data%>%filter(PCI.L<200)
-radon_data=radon_data%>%filter(PCI.L>0.04)
-radon_data$HOURS=as.numeric(as.character(radon_data$HOURS))
-radon_data=radon_data%>%filter(HOURS>24,HOURS<200)
+nc_lab_data$ZIPCODE=substr(nc_lab_data$POSTALCODE,1,5)
+nc_lab_data$PCI.L=as.numeric(as.character(nc_lab_data$PCI.L))
+nc_lab_data=nc_lab_data%>%filter(STATE%in%c("MA","NH","CT","ME","VT","RI"))
 
-##clean the date of MA and PA labs
-radon_data_sec$StartDate=as.character(radon_data_sec$StartDate)
-radon_data_sec$EndDate=as.character(radon_data_sec$EndDate)
+#clean the date data for MA lab
+ma_lab_data$StartDate=as.character(ma_lab_data$StartDate)
+ma_lab_data$EndDate=as.character(ma_lab_data$EndDate)
 
-radon_data_sec<-radon_data_sec%>%mutate(StartDate=ifelse(StartDate=="00/00/0000",NA,StartDate))
-radon_data_sec<-radon_data_sec%>%filter(!is.na(StartDate))
-radon_data_sec$StartDate<-as.Date(radon_data_sec$StartDate,"%m/%d/%Y")
+ma_lab_data<-ma_lab_data%>%mutate(StartDate=ifelse(StartDate=="00/00/0000",NA,StartDate))
+ma_lab_data<-ma_lab_data%>%filter(!is.na(StartDate))
+ma_lab_data$StartDate<-as.Date(ma_lab_data$StartDate,"%m/%d/%Y")
 
-radon_data_sec<-radon_data_sec%>%mutate(EndDate=ifelse(EndDate=="00/00/0000",NA,EndDate))
-radon_data_sec<-radon_data_sec%>%filter(!is.na(EndDate))
-radon_data_sec$EndDate<-as.Date(radon_data_sec$EndDate,"%m/%d/%Y")
+ma_lab_data<-ma_lab_data%>%mutate(EndDate=ifelse(EndDate=="00/00/0000",NA,EndDate))
+ma_lab_data<-ma_lab_data%>%filter(!is.na(EndDate))
+ma_lab_data$EndDate<-as.Date(ma_lab_data$EndDate,"%m/%d/%Y")
 
-radon_data_sec$PCI.L=as.numeric(as.character(radon_data_sec$Result))
-radon_data_sec[is.na(radon_data_sec$PCI.L),"PCI.L"]=0
-radon_data_sec=radon_data_sec%>%filter(PCI.L<200)
-radon_data_sec=radon_data_sec%>%filter(PCI.L>0.04)
+ma_lab_data$PCI.L=as.numeric(as.character(ma_lab_data$Result))
+ma_lab_data[is.na(ma_lab_data$PCI.L),"PCI.L"]=0
+ma_lab_data=ma_lab_data%>%filter(TestState%in%c("MA","NH","CT","ME","VT","RI"))
+#clean the date data for PA lab
+pa_lab_data$StartDate=as.character(pa_lab_data$StartDate)
+pa_lab_data$EndDate=as.character(pa_lab_data$EndDate)
 
-ne_radon=radon_data%>%filter(STATE%in%c("MA","NH","CT","ME","VT","RI"))
-ne_radon_sec=radon_data_sec%>%filter(TestState%in%c("MA","NH","CT","ME","VT","RI"))
-#only keep the records of basement
-ne_radon<-ne_radon%>%filter(FLOOR%in%c("basement","unknown"))
-ne_radon_sec<-ne_radon_sec%>%filter(Floor%in%c("Basement","Unknown"))
-#remove all records of "AFTERREPARIS" and the "FOLLOWUP" after that because it's always lower and not randomly sampled
-ne_repair=ne_radon%>%group_by(FINGERPRINT)%>%summarise(repair_status=ifelse("AFTERREPAIRS"%in%TESTTYPE,TRUE,FALSE))
-repair_date=ne_radon%>%
-  right_join(ne_repair%>%filter(repair_status))%>%
+pa_lab_data<-pa_lab_data%>%mutate(StartDate=ifelse(StartDate=="00/00/0000",NA,StartDate))
+pa_lab_data<-pa_lab_data%>%filter(!is.na(StartDate))
+pa_lab_data$StartDate<-as.Date(pa_lab_data$StartDate,"%m/%d/%Y")
+
+pa_lab_data<-pa_lab_data%>%mutate(EndDate=ifelse(EndDate=="00/00/0000",NA,EndDate))
+pa_lab_data<-pa_lab_data%>%filter(!is.na(EndDate))
+pa_lab_data$EndDate<-as.Date(pa_lab_data$EndDate,"%m/%d/%Y")
+
+pa_lab_data$PCI.L=as.numeric(as.character(pa_lab_data$Result))
+pa_lab_data[is.na(pa_lab_data$PCI.L),"PCI.L"]=0
+pa_lab_data=pa_lab_data%>%filter(TestState%in%c("MA","NH","CT","ME","VT","RI"))
+#select the measurements in period
+ma_lab_data$Year=lubridate::year(ma_lab_data$StartDate)
+pa_lab_data$Year=lubridate::year(pa_lab_data$StartDate)
+nc_lab_data$Year=lubridate::year(nc_lab_data$STARTDATE)
+
+ma_lab_data=ma_lab_data%>%filter(Year>1990,Year<2019)
+pa_lab_data=pa_lab_data%>%filter(Year>1990,Year<2019)
+nc_lab_data=nc_lab_data%>%filter(Year>1990,Year<2019)
+#select the basement measurments
+nc_lab_data<-nc_lab_data%>%filter(FLOOR%in%c("basement","unknown"))
+ma_lab_data<-ma_lab_data%>%filter(Floor%in%c("Basement","Unknown"))
+pa_lab_data<-pa_lab_data%>%filter(Floor%in%c("Basement","Unknown"))
+# select measurement with reasonable duractions
+nc_lab_data$HOURS=as.numeric(as.character(nc_lab_data$HOURS))
+nc_lab_data=nc_lab_data[!is.na(nc_lab_data$HOURS),]
+nc_lab_data<-nc_lab_data%>%filter(HOURS>71,HOURS<169)
+
+ma_lab_data=ma_lab_data%>%filter(Hours>47,Hours<97)
+pa_lab_data=pa_lab_data%>%filter(Hours>47,Hours<97)
+# select first time measurement
+nc_repair=nc_lab_data%>%group_by(FINGERPRINT)%>%summarise(repair_status=ifelse("AFTERREPAIRS"%in%TESTTYPE,TRUE,FALSE))
+repair_date=nc_lab_data%>%
+  right_join(nc_repair%>%filter(repair_status))%>%
   filter(TESTTYPE=="AFTERREPAIRS")%>%
   group_by(FINGERPRINT)%>%
   dplyr::select(FINGERPRINT,ENDDATE)%>%
   summarise(Repair_Date=min(ENDDATE))
 
 names(repair_date)=c("FINGERPRINT","Repair_Date")
-ne_radon=ne_radon%>%left_join(repair_date)
+nc_lab_data=nc_lab_data%>%left_join(repair_date)
 
-repaired=ne_radon%>%filter(!is.na(Repair_Date))
-un_repaired=ne_radon%>%filter(is.na(Repair_Date))
+repaired=nc_lab_data%>%filter(!is.na(Repair_Date))
+un_repaired=nc_lab_data%>%filter(is.na(Repair_Date))
 #in the repaired home, remove all records after repairment except real property check
 repaired=repaired%>%filter(!(ENDDATE>=Repair_Date & TESTTYPE%in%c("AFTERREPAIRS","FOLLOWUP")))
-ne_radon=bind_rows(repaired,un_repaired)
-ne_radon=ne_radon%>%filter%>%filter(lubridate::year(STARTDATE)>1990)
+nc_lab_data=bind_rows(repaired,un_repaired)
 
-#In MA&PA lab, no test type is provided, just select the first wave of measurements
-ne_radon_sec_date=ne_radon_sec%>%group_by(Checksum_TestAddress)%>%summarise(StartDate=min(StartDate))
-ne_radon_sec=ne_radon_sec_date%>%left_join(ne_radon_sec)
-ne_radon_sec=ne_radon_sec%>%filter%>%filter(lubridate::year(StartDate)>1990)
-ne_radon_sec=ne_radon_sec%>%filter(Hours>24,Hours<200)
+ma_lab_data_date=ma_lab_data%>%group_by(Checksum_TestAddress)%>%summarise(StartDate=min(StartDate))
+ma_lab_data=ma_lab_data_date%>%left_join(ma_lab_data)
 
+pa_lab_data_date=pa_lab_data%>%group_by(Checksum_TestAddress)%>%summarise(StartDate=min(StartDate))
+pa_lab_data=pa_lab_data_date%>%left_join(pa_lab_data)
+#Exclude measurments below detection limit
+nc_lab_data=nc_lab_data%>%filter(PCI.L>0.2,PCI.L<200)
+ma_lab_data=ma_lab_data%>%filter(PCI.L>0.4,PCI.L<200)
+pa_lab_data=pa_lab_data%>%filter(PCI.L>0.4,PCI.L<200)
+#Merge them together
+nc_lab_data$Type="AirChek"
+ma_lab_data$Type="AccuStar"
+pa_lab_data$Type="AccuStar"
+
+nc_lab_data=nc_lab_data[,c("STARTDATE","ENDDATE","ZIPCODE","STATE","COUNTY","CITY","PCI.L","FINGERPRINT","HOURS","Type")]
+ma_lab_data=ma_lab_data[,c("StartDate","EndDate","TestPostalCode","TestState","County","TestCity","PCI.L","Checksum_TestAddress","Hours","Type")]
+pa_lab_data=pa_lab_data[,c("StartDate","EndDate","TestPostalCode","TestState","County","TestCity","PCI.L","Checksum_TestAddress","Hours","Type")]
+names(ma_lab_data)=names(nc_lab_data)
+names(pa_lab_data)=names(nc_lab_data)
+nc_lab_data$FINGERPRINT=as.character(nc_lab_data$FINGERPRINT)
+ma_lab_data$FINGERPRINT=as.character(ma_lab_data$FINGERPRINT)
+pa_lab_data$FINGERPRINT=as.character(pa_lab_data$FINGERPRINT)
+
+ne_radon=bind_rows(nc_lab_data,ma_lab_data,pa_lab_data)
+#add month column and season column
 ne_radon$Month=lubridate::month(ne_radon$STARTDATE)
-ne_radon_sec$Month=lubridate::month(ne_radon_sec$StartDate)
+ne_radon$Year=lubridate::year(ne_radon$STARTDATE)
 
-#Change the name of ne_radon_sec to the names of ne_radon
-ne_radon=ne_radon[,c("STARTDATE","ENDDATE","ZIPCODE","STATE","COUNTY","CITY","LAB","Month","PCI.L","FINGERPRINT","HOURS")]
-ne_radon_sec=ne_radon_sec[,c("StartDate","EndDate","TestPostalCode","TestState","County","TestCity","Lab","Month","PCI.L","Checksum_TestAddress","Hours")]
-ne_radon_sec$Checksum_TestAddress=as.character(ne_radon_sec$Checksum_TestAddress)
-names(ne_radon_sec)=names(ne_radon)
-
-ne_radon=bind_rows(ne_radon,ne_radon_sec)
 #create a month season look up table
 trans<-cbind.data.frame(Month=1:12,
                         Season=c("Winter","Winter","Spring","Spring","Spring","Summer",
                                  "Summer","Summer","Autumn","Autumn","Autumn","Winter"))
 ne_radon=ne_radon%>%left_join(trans)
-ne_radon$Year=lubridate::year(ne_radon$STARTDATE)
 #The winter months (Jan Feb) in the new year are merged with previous year
 ne_radon$Season_Year=ifelse(ne_radon$Month<3,ne_radon$Year-1,ne_radon$Year)
 #the number of records in each zipcode
