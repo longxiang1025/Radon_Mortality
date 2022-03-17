4b744d82fb9df644258e4ec90c306feb63acfe69
Longxiang Li <longxiang1025@gmail.com>
2022-03-17 02:33:39 -0400

    Evaluate the performance of the first set of parameters
    10000_4_5000

--- Code/25.1_Aggregate_ST_RF_Models.R
+++ Code/25.1_Aggregate_ST_RF_Models.R
@@ -1,11 +1,14 @@
 library(dplyr)
 library(boot)
 
-files=list.files("/n/holyscratch01/koutrakis_lab/Users/loli/ST_RF_10000_2_5000/",recursive = T)
+#files=list.files("/n/holyscratch01/koutrakis_lab/Users/loli/ST_RF_10000_2_5000/",recursive = T)
+files=list.files("/n/holyscratch01/koutrakis_lab/Users/loli/ST_RF/",recursive = T)
+
 test_result=list()
 l=1
 for( f in files){
-  load(paste0("/n/holyscratch01/koutrakis_lab/Users/loli/ST_RF_10000_2_5000/",f))
+  #load(paste0("/n/holyscratch01/koutrakis_lab/Users/loli/ST_RF_10000_2_5000/",f))
+  load(paste0("/n/holyscratch01/koutrakis_lab/Users/loli/ST_RF/",f))
   test_result[[l]]=test
   l=l+1
 }
